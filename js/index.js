86c10150af9590608307d227f2a4e65afe6b4c43
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-07 18:53:08 -0400

    slowly but surely

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+document.querySelectorAll('.btn').forEach(element => {
+    element.addEventListener('mouseout', event => {
+        event.target.textContent = 'Tiff Tiff!!'
+    })
+})
+
+const navButton = document.querySelector('a')
+const secretPhoto = document.querySelector('.secret img')
+console.log(secretPhoto)
+navButton.addEventListener('click', () => {
+    document.body.innerHTML = ""
+    document.body.innerHTML = secretPhoto
+})
+
+// document.querySelector('nav').addEventListener('click', function (evt) {
+//     evt.preventDefault()
+// })                                       
