5399574f5840975d5bf54041ae3b2673e7195bc2
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-08 11:06:32 -0400

    .

--- js/index.js
+++ js/index.js
@@ -2,16 +2,36 @@
 
 document.querySelectorAll('.btn').forEach(element => {
     element.addEventListener('mouseout', event => {
-        event.target.textContent = 'Tiff Tiff!!'
+        event.target.textContent = 'OmegaLUL'
     })
 })
 
 const navButton = document.querySelector('a')
-const secretPhoto = document.querySelector('.secret img')
-console.log(secretPhoto)
 navButton.addEventListener('click', () => {
     document.body.innerHTML = ""
-    document.body.innerHTML = secretPhoto
+
+})
+
+document.querySelectorAll('.subHeader').forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.textContent = "POOF!"
+    })
+})
+document.querySelectorAll('p').forEach(element => {
+    element.addEventListener('dblclick', event => {
+        event.target.textContent = "POOF! AGAIN! HAHA!"
+    })
+})
+document.querySelectorAll('.logo-heading').forEach(element =>{
+    element.addEventListener('dblclick', eventObj =>{
+        eventObj.target.textContent = "This is not the title anymore!"
+    })
+})
+document.querySelectorAll('img').forEach(element => {
+    element.addEventListener('dragend', event => {
+        debugger
+        event.target.innerHTML = "Nice, you lost it"
+    })
 })
 
 // document.querySelector('nav').addEventListener('click', function (evt) {
